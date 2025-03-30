43bdbf2abbd30bff80d5ab7e8982c6e5e57df233
IuliiaSelivanova <yulya.danilina.96@inbox.ru>
2025-03-30 23:41:33 +0300

    уведичен лимит количества треков

--- src/scripts/utils/api.js
+++ src/scripts/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function fetchTracks() {
   const URL =
-    "https://api.jamendo.com/v3.0/tracks/?client_id=92a975eb&format=json&search=rock&limit=10";
+    "https://api.jamendo.com/v3.0/tracks/?client_id=92a975eb&format=json&search=rock&limit=20";
 
   const res = await axios.get(URL);
   return res.data.results;
